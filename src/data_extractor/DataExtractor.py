917f51291a8951b9c208adc136610a834fbaba94
Adnan Habib Rahin <adnan.rahin13@gmail.com>
2024-06-15 21:03:01 -0400

    added new extract methods

--- src/data_extractor/DataExtractor.py
+++ src/data_extractor/DataExtractor.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, lower, avg, row_number
+from pyspark.sql.functions import col, lower, avg, countDistinct, row_number
 from pyspark.sql.window import Window
 from pyspark.storagelevel import StorageLevel
 
@@ -35,6 +35,38 @@ class DataExtractor:
     def statewise_male_female_count(self) -> DataFrame:
         male_female_count_df = self.person_domain_df.groupBy("state", "gender").count().alias("count")
         pivot_df = male_female_count_df.groupBy("state").pivot("gender").sum("count")
+        pivot_df.persist(StorageLevel.MEMORY_AND_DISK)
+        return pivot_df
+
+    def average_age_each_state(self) -> DataFrame:
+        result_df = self.person_domain_df.groupBy("state").agg(avg(col("age")).alias("average_age"))
+        result_df.persist(StorageLevel.MEMORY_AND_DISK)
+        return result_df
+
+    def top_states_by_persons(self, top_n: int = 5) -> DataFrame:
+        state_count_df = self.person_domain_df.groupBy("state").count().alias("total_persons")
+        top_states_df = state_count_df.orderBy(col("count").desc()).limit(top_n)
+        top_states_df.persist(StorageLevel.MEMORY_AND_DISK)
+        return top_states_df
+
+    def count_unique_ips_per_state(self) -> DataFrame:
+        ip_counts_df = self.person_domain_df.groupBy("state").agg(
+            countDistinct(col("ipV4")).alias("unique_ipV4_count"),
+            countDistinct(col("ipV6")).alias("unique_ipV6_count")
+        )
+        ip_counts_df.persist(StorageLevel.MEMORY_AND_DISK)
+        return ip_counts_df
+
+    def find_persons_with_valid_emails(self) -> DataFrame:
+        valid_email_df = self.person_domain_df.filter(
+            col("email").rlike("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+        )
+        valid_email_df.persist(StorageLevel.MEMORY_AND_DISK)
+        return valid_email_df
+
+    def citywise_gender_distribution(self) -> DataFrame:
+        gender_count_df = self.person_domain_df.groupBy("city", "gender").count().alias("gender_count")
+        pivot_df = gender_count_df.groupBy("city").pivot("gender").sum("count")
         pivot_df.persist(StorageLevel.MEMORY_AND_DISK)
         return pivot_df
 
