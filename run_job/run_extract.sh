e687c7622776a60e925cb5229c7942ef70f17a85
Adnan Habib Rahin <adnan.rahin13@gmail.com>
2024-06-15 21:21:59 -0400

    update

--- run_job/run_extract.sh
+++ run_job/run_extract.sh
@@ -4,20 +4,29 @@
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 SRC_DIR="$PROJECT_DIR/src"
 LIB_DIR="$PROJECT_DIR/lib"
+LOG_DIR="$PROJECT_DIR/logs"
 SPARK_JOB_ZIP="spark_job.zip"
-LOG_FILE="../logs/spark_job.log"
+LOG_FILE="$LOG_DIR/spark_job.log"
+
+# Ensure log directory exists
+mkdir -p "$LOG_DIR"
+
 # Print current working directory for debugging
 echo "Current working directory: $PROJECT_DIR"
 
 # Change to source directory
-cd "$SRC_DIR" || exit
+cd "$SRC_DIR" || { echo "Error: Unable to change to source directory $SRC_DIR"; exit 1; }
 
 # Zip Python files
 echo "Zipping Python files..."
-zip -r "$LIB_DIR/$SPARK_JOB_ZIP" ./*.py ./data_loader/*.py ./data_writer/*.py ./data_extractor/*.py
+zip -r "$LIB_DIR/$SPARK_JOB_ZIP" ./*.py ./data_loader/*.py ./data_writer/*.py ./data_extractor/*.py > "$LOG_FILE" 2>&1
+if [ $? -ne 0 ]; then
+  echo "Error zipping Python files. Check $LOG_FILE for details."
+  exit 1
+fi
 
 # Change back to the original directory
-cd - || exit
+cd - || { echo "Error: Unable to change back to the original directory"; exit 1; }
 
 # Submit Spark job
 echo "Submitting Spark job..."
@@ -43,7 +52,7 @@ spark-submit \
   --extract_name find_persons_with_valid_emails \
   --extract_name citywise_gender_distribution \
   --extract_name find_people_under_same_public_ip4 \
-  --extract_name count_total_iid_each_state > "$LOG_FILE" 2>&1
+  --extract_name count_total_iid_each_state >> "$LOG_FILE" 2>&1
 
 # Check if Spark job completed successfully
 if [ $? -eq 0 ]; then
